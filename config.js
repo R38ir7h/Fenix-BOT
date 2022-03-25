37a61de3e599fd6691dbb7206a48ae0091681a98
R38ir7h <102347701+R38ir7h@users.noreply.github.com>
2022-03-25 10:21:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ module.exports = {
    "description": "",
    "domain":"https://Fenix-bot.forsakenworld.repl.co",
    "google_analitics": "", 
-   "token": "OTU2MDU0NTA4Mzk0Mzg1NDE4.YjqpJQ.nCpNEeLquXcJnpxwJmcQKfMtc8U",
+   "token": "TOKEN",
    "https":"https://Fenix-bot.forsakenworld.repl.co",
    "port":"5003",
    "client_id":"956054508394385418",
-   "secret":"eyfy7HxwGoBll4e4yO3dnh2eE2324ItO",
+   "secret":"SECRET",
    "support_server_link":"https://discord.gg/zKu58KSPTh",
    "bot_name":"Феникс"
   }
