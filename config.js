230459343ae50f580ecc8a5d7cf5bdb36985855f
R38ir7h <102347701+R38ir7h@users.noreply.github.com>
2022-03-28 08:34:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
    "https":"https://fenix-bot-rebirth.herokuapp.com",
    "port":"5003",
    "client_id":"956054508394385418",
-   "secret":"SECRET",
+   "secret":"eyfy7HxwGoBll4e4yO3dnh2eE2324ItO",
    "support_server_link":"https://discord.gg/zKu58KSPTh",
    "bot_name":"Феникс"
   }
