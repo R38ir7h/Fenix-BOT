dded69b876519247515bea2e0d3271f782433af1
R38ir7h <102347701+R38ir7h@users.noreply.github.com>
2022-03-28 08:18:54 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -3,7 +3,6 @@ const logger = require('./utils/logger');
 const Discord = require('discord.js');
 const Statcord = require("statcord.js");
 const { token } = require('./utils/variables.js');
-const client = new Discord.client();
 
 
 const manager = new Discord.ShardingManager('./index.js', {
@@ -12,7 +11,7 @@ const manager = new Discord.ShardingManager('./index.js', {
   //totalShards: 'auto'
   totalShards: 1
 });
-client.login(process.env.TOKEN);
+
 
 manager.spawn();
 
